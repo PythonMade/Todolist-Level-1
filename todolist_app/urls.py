41a65027c4cef97fcdf20887fae1ebd369ee7cf0
PythonMade <ronald@pythonmade.com>
2020-07-21 14:54:06 -0400

    add archive page and update navbar

--- todolist_app/urls.py
+++ todolist_app/urls.py
@@ -19,4 +19,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='todolist-home'),
+    path('archive/', views.archive, name='todolist-archive'),
 ]
--- todolist_app/views.py
+++ todolist_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'todolist_app/home.html')
+
+def archive(request):
+    return render(request, 'todolist_app/archive.html')
