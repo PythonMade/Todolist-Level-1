890251b464e034b9d257208308f8d0497c19420a
PythonMade <ronald@pythonmade.com>
2020-07-21 15:55:54 -0400

    updated css styles

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
 
+tasks = [
+    {
+        'title': 'Task 1',
+        'is_achived': False,
+        'date_created': 'July 1, 2020',
+    },
+    {
+        'title': 'Task 2',
+        'is_achived': False,
+        'date_created': 'July 1, 2020',
+    },
+    {
+        'title': 'Task 3',
+        'is_achived': False,
+        'date_created': 'July 1, 2020',
+    },
+    {
+        'title': 'Task 4',
+        'is_achived': False,
+        'date_created': 'July 1, 2020',
+    },
+    {
+        'title': 'Task 5',
+        'is_achived': False,
+        'date_created': 'July 1, 2020',
+    },
+]
+
 def home(request):
-    return render(request, 'todolist_app/home.html')
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, 'todolist_app/home.html', context)
 
 def archive(request):
     return render(request, 'todolist_app/archive.html')
